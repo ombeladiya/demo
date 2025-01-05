21cb7c4bf543a99063d7b6ad5effe84af09afe16
ombeladiya <ombeladiya1854@gmail.com>
2025-01-05 12:33:44 +0530

    list added

--- om.jsx
+++ om.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 function om() {
     return (
-        <div>
-            Hello GUty
-        </div>
+        <>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Conatct</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+        </>
     )
 }
 
